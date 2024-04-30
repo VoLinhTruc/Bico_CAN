b3101e2a48dc60dd831664e306f78d6053f75df9
VOR5HC <truc.volinh@vn.bosch.com>
2024-04-30 20:24:26 +0700

    update python code

---
+++ python_code/python_can_remake_1.py
@@ -0,0 +1,20 @@
+
+import time
+import threading
+
+import can
+
+bus = can.Bus(interface="serial", channel="COM4")
+
+while (1):
+    tx_msg = can.Message(
+                arbitration_id=0x712,
+                data=[0x30, 0xAA, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37],
+            )
+    bus.send(tx_msg)
+    
+    rx_msg = bus.recv(1)
+    if rx_msg is not None:
+        print(f"rx: {rx_msg}")
+    
+    time.sleep(1)
